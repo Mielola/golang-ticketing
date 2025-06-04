ce3e3376834734956c0113cd3747236bc30a1312
Mielola <mwildab15@gmail.com>
2025-06-04 16:55:42 +0700

    build update 0406

--- src/database/database.go
+++ src/database/database.go
@@ -13,7 +13,7 @@ var DB *gorm.DB
 const (
 	DBUser     = "kris"
 	DBPassword = "kris1234"
-	DBHost     = "localhost"
+	DBHost     = "super_apps"
 	DBPort     = "3306"
 	DBName     = "commandcenter"
 )
